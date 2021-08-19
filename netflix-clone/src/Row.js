2b3bccca4b72d6bcb609415aff6d44754c0e6bd6
Mohit Bhavsar <mohitbhavsar707@gmail.com>
2021-08-19 19:09:34 -0400

    Setup for axios

---
+++ netflix-clone/src/Row.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Row() {
+    return (
+        <div>
+            {/* title */ }
+            {/* container -> posters */}
+        </div>
+    )
+}
+
+export default Row
