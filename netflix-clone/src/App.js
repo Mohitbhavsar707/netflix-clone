76523d8112abdad16e78468caa8f7ec20d84263a
Mohit Bhavsar <mohitbhavsar707@gmail.com>
2021-08-23 11:22:34 -0400

    Worked on Row js

--- netflix-clone/src/App.js
+++ netflix-clone/src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import './App.css';
 import Row from './Row';
+import requests from './requests';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello!</h1>
-      <Row title ="NETFLIX ORIGINALS"/>
-      <Row title = "TRENDING NOW"/>
+      <h1>Hello this is the beginning of this React App!</h1>
+      <Row title ="NETFLIX ORIGINALS" fetchUrl={requests.fetchNetflixOriginals}/>
+      <Row title = "TRENDING NOW" fetchUrl={requests.fetchTrending}/>
       
     </div>
   );
--- netflix-clone/src/Row.js
+++ netflix-clone/src/Row.js
@@ -1,12 +1,38 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
 
-function Row() {
+function Row({title, fetchUrl}) {
+    const [movies, setMovies] = useState([])
+
+    // A snippet of code which runs based on a specific condition variable 
+    useEffect(() => {
+        async function fetchData(){
+            const request = await axios.get(fetchUrl);
+            console.log(request.data.results);
+            setMovies(request.data.results);
+            return request;
+
+        }
+        fetchData();
+
+    }, [fetchUrl]);
+
+    console.table(movies);
+
     return (
-        <div>
-            {/* title */ }
-            {/* container -> posters */}
+        <div className="row">
+            <h2>{title}</h2>
+
+            <div className="row_posters">
+                {/* several row_posters */}
+                {movies.map(movie => (
+                    <img src={movie.poster_path} alt={movie.name} />
+
+                ))}
+            </div>
         </div>
     )
 }
 
-export default Row
+export default Row;
+ 
